b6cc3ab1b3cc42a7cd323c14427d09ca518feb5a
Eric McCormick <edm00se@gmail.com>
2018-05-08 12:55:25 -0500

    style and layout improvements

---
+++ .vuepress/config.js
@@ -0,0 +1,34 @@
+const env = process.env.NODE_ENV || 'dev';
+
+const config = {
+    title: 'Awesome Board Games',
+    description: 'a curated list of awesome board games',
+    base: '/',
+    themeConfig: {
+        repo: 'https://github.com/edm00se/awesome-board-games/blob/master/contributing.md',
+        repoLabel: 'Add your favorite game!',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Contributing', link: '/contributing.html' },
+            { text: 'More...', items: [
+                { text: 'Formatting', link: '/formatting.html' },
+                { text: 'Help', link: '/help.html' },
+                { text: 'Code of Conduct', link: '/code-of-conduct.html' }
+            ] }
+        ],
+        sidebar: [
+            '/',
+            '/contributing.html',
+            '/formatting.html',
+            '/help.html',
+            '/code-of-conduct.html'
+        ]
+    }
+};
+
+if('production' === env){
+    // TODO: Google Analytics ID
+    config.ga = undefined;
+}
+
+module.exports = config;
