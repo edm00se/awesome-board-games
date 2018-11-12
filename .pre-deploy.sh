a6d3feaae531694fdae3ba15eef1f154264d7dab
Eric McCormick <edm00se@gmail.com>
2018-11-12 13:37:57 -0600

    build(now): migrates to now.sh v2
    no more manual Dockerfile config and unifies the finagling of the readme front matter with both travis and now
    
    fixes #42

---
+++ .pre-deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# enforces vuepress front matter without polluting 'source' readme.md
+mv readme.md alt.md &&\
+SPACE=' ' &&\
+echo -e "---\nhome:${SPACE}true\nsidebar:${SPACE}true\n---" > readme.md &&\
+cat alt.md >> readme.md &&\
+rm alt.md
+
