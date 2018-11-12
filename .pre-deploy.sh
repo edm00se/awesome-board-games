0e34101d5fa6d82d8272da0a1de4b8b055f21ff8
Eric McCormick <edm00se@gmail.com>
2018-11-12 13:42:40 -0600

    build(front-matter): fixes front-matter addition

--- .pre-deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# enforces vuepress front matter without polluting 'source' readme.md
-mv readme.md alt.md &&\
-SPACE=' ' &&\
-echo -e "---\nhome:${SPACE}true\nsidebar:${SPACE}true\n---" > readme.md &&\
-cat alt.md >> readme.md &&\
-rm alt.md
-
